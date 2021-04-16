5402c311746a475a59b23450d93a351b9ecf5cbe
Rodrigo Busto <rodrigo.busto@cerc.inf.br>
2021-04-16 16:46:38 -0300

    Criando boiler plate do projeto

---
+++ web-continuada02/src/main/java/com/bandtec/webcontinuada02/WebContinuada02Application.java
@@ -0,0 +1,13 @@
+package com.bandtec.webcontinuada02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebContinuada02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebContinuada02Application.class, args);
+	}
+
+}
---
+++ web-continuada02/src/test/java/com/bandtec/webcontinuada02/WebContinuada02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandtec.webcontinuada02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebContinuada02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
